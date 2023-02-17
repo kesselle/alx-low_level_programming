fce2730bc5185e4987a5dadc1e173fe72e73d01b
kesselle <kessellegahimbare@gmail.com>
2023-02-17 12:07:48 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char letter = 'a'; /* start with the letter 'a' */
-
-    while (letter <= 'z')
-    {
-        putchar(letter);
-        letter++;
-    }
-
-    putchar('\n');
-
-    return 0;
+char letter = 'a'; /* start with the letter 'a' */
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
 }

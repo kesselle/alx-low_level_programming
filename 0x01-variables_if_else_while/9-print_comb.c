6394e2b28f10ce32814d90a5d8fff9e80909069a
kesselle <kessellegahimbare@gmail.com>
2023-02-17 13:46:42 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,19 +6,14 @@
  */
 int main(void)
 {
-int i, j;
-for (i = 0; i < 10; i++)
+int num;
+for (num = 0; num < 10; num++)
 {
-for (j = i + 1; j < 10; j++)
-{
-putchar(i + '0');
-putchar(j + '0');
-if (i != 8 || j != 9)
+putchar(num + '0');
+if (num != 9)
 {
 putchar(',');
 putchar(' ');
-}
-}
 }
 }
 putchar('\n');

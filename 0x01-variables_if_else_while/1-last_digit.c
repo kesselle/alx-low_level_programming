43c3316708bf9c00e43c0129e23f4af313978e91
kesselle <kessellegahimbare@gmail.com>
2023-02-17 11:08:44 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,13 +2,13 @@
  * main - Prints the last digit of a randomly generated number
  * and whether it is greater than 5, than 6, or 0
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
 int n;
- srand(time(0));
- n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
 if ((n % 10) > 5)
 {

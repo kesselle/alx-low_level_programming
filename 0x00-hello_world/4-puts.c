13ee5311382c09d5c9b2490795d71aae2974ffe9
kesselle <kessellegahimbare@gmail.com>
2023-02-18 10:29:23 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- *
  * Description: Prints a string to the standard output stream
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle\n");

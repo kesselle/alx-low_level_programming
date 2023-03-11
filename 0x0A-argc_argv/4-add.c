60299c04c55e999964059929cd44f2b75050adbe
kesselle <kessellegahimbare@gmail.com>
2023-03-10 16:00:29 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -35,30 +35,6 @@ return (1);
 int main(int argc, char *argv[])
 {
 /*Declaring variables*/
-unsigned int count;
-count = 0;
-while (count < strlen(str)) /*count string*/
-{
-if (!isdigit(str[count])) /*check if str there are digit*/
-{
-return (0);
-}
-count++;
-}
-return (1);
-}
-/**
-* main - Print the name of the program
-* @argc: Count arguments
-* @argv: Arguments
-*
-*
-* *
-* Return: Always 0 (Success)
-*/
-int main(int argc, char *argv[])
-{
-/*Declaring variables*/
 int count;
 int str_to_int;
 int sum = 0;
@@ -81,4 +57,3 @@ count++;
 printf("%d\n", sum); /*print sum*/
 return (0);
 }
-
